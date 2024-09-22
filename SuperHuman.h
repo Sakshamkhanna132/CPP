20ef989b8aaa122ba20e673c062908dffe0e8d27
sharp119 <cs.aditya58@gmail.com>
2024-09-22 08:14:37 +0530

    superhuman extension

---
+++ SuperHuman.h
@@ -0,0 +1,35 @@
+#ifndef SUPERHUMAN_H
+#define SUPERHUMAN_H
+
+#include "Human.h"
+#include <iostream>
+using namespace std;
+
+class SuperHuman : Human {
+
+private:
+
+    string planet;
+    string environment;
+    int superPowerLevel;
+    string specialAbility;
+
+public:
+
+    SuperHuman(string name, int age, double height, string planet, string environment, int superPowerLevel, string specialAbility);
+
+    string getPlanet() const;
+    string getEnvironment() const;
+    int getSuperPowerLevel() const;
+    string getSpecialAbility() const;
+
+    void setPlanet(string planet);
+    void setEnvironment(string environment);
+    void setSuperPowerLevel(int superPowerLevel);
+    void setSpecialAbility(string specialAbility);
+
+
+
+};
+
+#endif
